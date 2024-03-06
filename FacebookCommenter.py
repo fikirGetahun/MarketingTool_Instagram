73ef6dfd9cec345c22657e723d6729a81521b9d1
fikirGetahun <fikirgetahun97@gmail.com>
2024-03-06 16:53:22 +0300

    collecting xpath skeleton

--- FacebookCommenter.py
+++ FacebookCommenter.py
@@ -17,6 +17,12 @@ def commenter():
     passxpath.send_keys("Qwe@1234567")
     loginxpath.click()
 
+    ### this is inside a specfic page list of posts
+    comment_field_skeleton_facebook_PAGE = """/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[3]/div[1]/div/div/div/div/div/div/div/div/div/div/div[8]/div/div/div[4]/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/form/div/div/div[1]/div/div[1]/p"""
+    ### differences occures at string possitons 28, 46, 64, 92, 110, 128, 158, 176, 194
+    ### all we have to do is changing the number loop itrations on those numbers 1... up to whatever
+
+    
     commentLineXpath = ''
 
     time.sleep(15)
